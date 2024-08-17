a8cef32fbed96ff4d5070eb51caa6dc2fcb11421
IT24102513 <it24102513@my.sliit.lk>
2024-08-18 00:12:19 +0530

    Update IT24102513Lab4Q1.java

--- Q1/IT24102513Lab4Q1.java
+++ Q1/IT24102513Lab4Q1.java
@@ -6,10 +6,10 @@ public static void main(String[] args) {
 
   Scanner input = new Scanner(System.in);
   
-  int number;
+   int number;
 
-  System.out.print("Enter a number : " );
-  number = input.nextInt();
+    System.out.print("Enter a number : " );
+    number = input.nextInt();
 
   if (number > 0) {
      System.out.println("The number is: Positive ");
